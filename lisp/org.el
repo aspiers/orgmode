7b3860a81485c8e4d05999b8c01a958d50424364
Adam Spiers <orgmode@adamspiers.org>
2009-07-11 23:10:34 +0100

    org-map-entries: return a sub-list for each file handled

--- lisp/org.el
+++ lisp/org.el
@@ -11178,10 +11178,12 @@ Returns the new tags string, or nil to not change the current settings."
 (defun org-map-entries (func &optional match scope &rest skip)
   "Call FUNC at each headline selected by MATCH in SCOPE.
 
-FUNC is a function or a lisp form.  The function will be called without
-arguments, with the cursor positioned at the beginning of the headline.
-The return values of all calls to the function will be collected and
-returned as a list.
+FUNC is a function or a lisp form.  The function will be called
+without arguments, with the cursor positioned at the beginning of the
+headline.  The return values of all calls to the function will be
+collected and returned as a list, although if several files are
+iterated over, a list of sub-lists will be returned: one sub-list of
+results for each file.
 
 The call to FUNC will be wrapped into a save-excursion form, so FUNC
 does not need to preserve point.  After evaluaton, the cursor will be
@@ -11274,13 +11276,14 @@ a *different* entry, you cannot use these techniques."
 	   ((eq scope 'file-with-archives)
 	    (setq scope (org-add-archive-files (list (buffer-file-name))))))
 	  (org-prepare-agenda-buffers scope)
-	  (while (setq file (pop scope))
-	    (with-current-buffer (org-find-base-buffer-visiting file)
-	      (save-excursion
-		(save-restriction
-		  (widen)
-		  (goto-char (point-min))
-		  (setq res (append res (org-scan-tags func matcher))))))))))
+	  (mapcar (lambda (file)
+                    (with-current-buffer (org-find-base-buffer-visiting file)
+                      (save-excursion
+                        (save-restriction
+                          (widen)
+                          (goto-char (point-min))
+                          (setq res (append res (org-scan-tags func matcher)))))))
+                  scope))))
     res))
 
 ;;;; Properties
